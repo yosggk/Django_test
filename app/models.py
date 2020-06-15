e0d2d5e253b88e27f653fac8afd0f60854d6be6e
1sggk1 <yo1sggk1@pm.me>
2020-06-15 23:40:15 +0900

    add

--- app/models.py
+++ app/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-
 class Article(models.Model):
     content = models.CharField(max_length=200)
-    
+    user_name = models.CharField(max_length=200, null=True)
     def __str__(self):
         return self.content
