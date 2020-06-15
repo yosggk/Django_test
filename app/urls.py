a64ca9b5bcac2c55074d1dbf7fd76d0af81c2758
1sggk1 <yo1sggk1@pm.me>
2020-06-16 00:08:16 +0900

    add

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:id>', views.detail, name='detail'),
+    path('create', views.create, name='create'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Article
 
@@ -6,6 +6,24 @@ def index(request):
     articles = Article.objects.all()
     context = {
         'message': 'Welcome my BBS',
+        'articles': articles,
+    }
+    return render(request, 'app/index.html', context)
+
+def detail(request, id):
+    article = get_object_or_404(Article, pk=id)
+    context = {
+        'message': 'Show Article ' + str(id),
+        'article': article,
+    }
+    return render(request, 'app/detail.html', context)
+
+def create(request):
+    article =Article(content='Hello BBS', user_name='paiza')
+    article.save()
+    articles = Article.objects.all()
+    context = {
+        'message': 'Create article',
         'articles': articles,
     }
     return render(request, 'app/index.html', context)
